5aa6c957ab5f9775a31179844d702851b4baaef6
Mónica Polanco <mpolan16@g.holycross.edu>
2020-07-10 17:51:31 -0400

    added api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,28 @@
+const Workout = require("../models/workout.js");
+module.exports = function(app){
+    app.get("/api/workouts",(req,res)=>{
+        Workout.find({}).then(data=>{
+            res.json(data);
+        });
+    });
+    app.put("/api/workouts/:id",(req,res)=>{
+        Workout.update({_id:mongoose.Types.ObjectId(req.params.id)},{$push:{exercises:req.body}},(err, data)=>{
+            if(err){
+                res.send(err)
+            }else{
+                res.json(data)
+            }
+        });
+    });
+    app.post("/api/workouts", (req,res)=>{
+        Workout.create({}).then(data=>{
+            res.json(data);
+        });
+    });
+    app.get("/api/workouts/range",(req,res)=>{
+        // sending only last 7 workouts with .limit(7)
+        Workout.find({}).limit(7).then(data=>{
+            res.json(data);
+        });
+    });
+}
