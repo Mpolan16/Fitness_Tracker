5e8e1444061bb091d74eb0796ca3173b9d0a97b4
Mónica Polanco <mpolan16@g.holycross.edu>
2020-07-10 17:53:07 -0400

    created the workout schema

---
+++ models/workout.js
@@ -0,0 +1,29 @@
+// working backwards from seeds file
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const WorkoutSchema = new Schema(
+{
+    day:{
+        type: Date,
+        default: Date.now
+    },
+    exercises:[
+        {
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number
+        }
+    ]
+},{
+    toJSON:{
+        virtuals:true
+    }
+}
+);
+
+const Workout = mongoose.model("Workout",WorkoutSchema);
+module.exports = Workouts;
