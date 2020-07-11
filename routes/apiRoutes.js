326053a3e785b01f69a243271d32d4840a3e8b85
Mónica Polanco Fabián <Mpolan16@g.holycross.edu>
2020-07-11 10:02:36 -0400

    cleaned up code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,14 +8,11 @@ module.exports = function(app){
         });
     });
     app.put("/api/workouts/:id",(req,res)=>{
-        console.log(req.body)
-        Workout.findByIdAndUpdate(req.params.id,{$push:{exercises:req.body}},{new: true, runValidators: true})
+        Workout.findByIdAndUpdate(req.params.id,{$push:{exercises:req.body}})
         .then((data)=>{
-                console.log(data)
                 res.json(data)
             }
         ).catch(err=>{
-            console.log(err);
             res.json(err);
         })
     });
